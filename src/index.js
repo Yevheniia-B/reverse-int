e0802a36ab8bf95cea4d6b1433f0d984389e030b
Yevheniia-B <evgenijar.work@gmail.com>
2020-10-24 21:17:38 +0300

    feat: reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+       
+    if (String(n).startsWith('-')) {
+       return String(n).split('').reverse().join('').slice(0, -1);
+    }
+    else {
+      return String(n).split('').reverse().join('')
+    }
 }
